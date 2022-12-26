33c9b81bccf85c8bd2a20d754c813f87f4cd8698
Moddingdudes <halestorm512@gmail.com>
2022-12-26 00:30:25 -0500

    Add boolean support

---
+++ Standard/Editor/BoolReferenceDrawer.cs
@@ -0,0 +1,10 @@
+using UnityEditor;
+
+namespace CyberneticStudios.SOFramework
+{
+    [CustomPropertyDrawer(typeof(BoolReference))]
+    public class BoolReferenceDrawer : ReferenceDrawer<BoolReference>
+    {
+
+    }
+}
---
+++ Standard/Variables/BoolVariable.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace CyberneticStudios.SOFramework
+{
+    [CreateAssetMenu(menuName = AssetMenu.Variables + "Bool Variable")]
+    public class BoolVariable : Variable<bool>
+    {
+
+    }
+}
