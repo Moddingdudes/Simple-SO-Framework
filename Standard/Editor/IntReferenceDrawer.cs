186536154f30c475cfc484423c33c9b908d74321
Moddingdudes <halestorm512@gmail.com>
2023-01-10 21:52:16 -0500

    Add reference drawer for int

---
+++ Standard/Editor/IntReferenceDrawer.cs
@@ -0,0 +1,10 @@
+using UnityEditor;
+
+namespace CyberneticStudios.SOFramework
+{
+    [CustomPropertyDrawer(typeof(IntReference))]
+    public class IntReferenceDrawer : ReferenceDrawer<int>
+    {
+
+    }
+}
