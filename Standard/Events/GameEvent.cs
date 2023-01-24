7014d99f651eb3e3ac91850af19473b886a242f5
Moddingdudes <halestorm512@gmail.com>
2023-01-24 16:16:48 -0500

    Add 2 type generics to events

--- Standard/Events/GameEvent.cs
+++ Standard/Events/GameEvent.cs
@@ -45,4 +45,48 @@ namespace CyberneticStudios.SOFramework
             }
         }
     }
+
+    /// <summary>
+    /// Base class of all Event Channels. Event channels are scriptable objects that raise events and can be listened
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class GameEvent<T1, T2> : ScriptableObject
+    {
+
+#if UNITY_EDITOR
+        [Multiline]
+        public string DeveloperDescription = "";
+#endif
+
+        public System.Action<T1, T2> OnGameEventRaised;
+        [SerializeField] DebugInformation debugInformation;
+
+        public void RaiseEvent(T1 valueOne, T2 valueTwo)
+        {
+            OnGameEventRaised?.Invoke(valueOne, valueTwo);
+
+            if (debugInformation.debugEnabled)
+            {
+                //Debug information
+
+                string debugMessage = debugInformation.debugMessage;
+
+                if (debugInformation.includeData)
+                    debugMessage += valueOne + " " + valueTwo;
+
+                switch (debugInformation.debugLevel)
+                {
+                    case DebugInformation.DebugLevel.INFO:
+                        Debug.Log(debugMessage);
+                        break;
+                    case DebugInformation.DebugLevel.WARNING:
+                        Debug.LogWarning(debugMessage);
+                        break;
+                    case DebugInformation.DebugLevel.ERROR:
+                        Debug.LogError(debugMessage);
+                        break;
+                }
+            }
+        }
+    }
 }
--- Standard/Events/GameEventListener.cs
+++ Standard/Events/GameEventListener.cs
@@ -38,4 +38,40 @@ namespace CyberneticStudios.SOFramework
             Response.Invoke(value);
         }
     }
+
+    /// <summary>
+    /// Base class of all Event Listeners. Event listeners listen in on Event Channels for editor responses
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class GameEventListener<T1, T2> : MonoBehaviour
+    {
+        // Generic Unity Event
+        [System.Serializable] public class UnityGameEvent : UnityEvent<T1, T2> { }
+
+        [SerializeField] GameEvent<T1, T2> GameEvent;
+        [SerializeField] UnityGameEvent Response;
+
+        private void OnEnable()
+        {
+            //Check if the event exists to avoid errors
+            _ = GameEvent ?? throw new MissingReferenceException();
+
+            GameEvent.OnGameEventRaised += OnResponseEventRaised;
+        }
+
+        private void OnDisable()
+        {
+            _ = GameEvent ?? throw new MissingReferenceException();
+
+            GameEvent.OnGameEventRaised -= OnResponseEventRaised;
+        }
+
+        public void OnResponseEventRaised(T1 valueOne, T2 valueTwo)
+        {
+            //TODO: This doesn't appear to be thrown when the UnityEvent isn't set in the inspector and the event is fired
+            _ = Response ?? throw new MissingReferenceException();
+
+            Response.Invoke(valueOne, valueTwo);
+        }
+    }
 }
