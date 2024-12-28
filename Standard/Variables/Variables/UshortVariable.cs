1e55a23055f212095dc0690499581af9ee47b18b
Moddingdudes <halestorm512@gmail.com>
2024-12-28 11:40:14 -0500

    feat: add ushort

---
+++ Standard/Variables/Variables/UshortVariable.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace CyberneticStudios.SOFramework
+{
+    [CreateAssetMenu(menuName = AssetMenu.Variables + "Ushort Variable")]
+    public class UshortVariable : Variable<ushort>
+    {
+
+    }
+}
